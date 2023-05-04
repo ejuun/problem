ae1712a6bb751255eb03d938b4c15d806cd922c9
ejuun <122511545+ejuun@users.noreply.github.com>
2023-05-04 23:14:57 +0900

    [Gold IV] Title: 해킹, Time: 728 ms, Memory: 147080 KB -BaekjoonHub

---
+++ 백준/Gold/10282. 해킹/해킹.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+for _ in range(int(input())):
+    n, d, c = map(int, input().split())
+
+    G = [[] for _ in range(n + 1)]
+
+    for _ in range(d):
+        a, b, s = map(int, input().split())
+        G[b].append((a, s))
+
+    INF = int(1e9)
+
+    queue = deque()
+    queue.append(c)
+    visited = [INF] * (n + 1)
+    visited[c] = 0
+
+    while queue:
+        x = queue.popleft()
+
+        for p, time in G[x]:
+            if visited[p] > visited[x] + time:
+                visited[p] = visited[x] + time
+                queue.append(p)
+
+    cnt = 0
+    total = 0
+    for time in visited:
+        if time != INF:
+            cnt += 1
+            if total < time:
+                total = time
+
+    print(cnt, total)
