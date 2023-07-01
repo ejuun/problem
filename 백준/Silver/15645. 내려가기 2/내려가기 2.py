05e6cb4bb8beb92cf6ad0e8797fe10a2ceb40b5c
ejuun <122511545+ejuun@users.noreply.github.com>
2023-07-01 23:48:29 +0900

    [Silver I] Title: 내려가기 2, Time: 332 ms, Memory: 136200 KB -BaekjoonHub

---
+++ 백준/Silver/15645. 내려가기 2/내려가기 2.py
@@ -0,0 +1,23 @@
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+mini = [[0 for _ in range(3)] for _ in range(N)]
+
+for i in range(N):
+    for j in range(3):
+        mini[i][j] = arr[i][j]
+
+for i in range(1, N):
+    for j in range(3):
+        if j == 0:
+            arr[i][j] += max(arr[i-1][j], arr[i-1][j+1])
+            mini[i][j] += min(mini[i - 1][j], mini[i - 1][j + 1])
+
+        elif j == 1:
+            arr[i][j] += max(arr[i-1][j-1], arr[i - 1][j], arr[i - 1][j + 1])
+            mini[i][j] += min(mini[i - 1][j - 1], mini[i - 1][j], mini[i - 1][j + 1])
+
+        else:
+            arr[i][j] += max(arr[i - 1][j - 1], arr[i - 1][j])
+            mini[i][j] += min(mini[i - 1][j - 1], mini[i - 1][j])
+
+print(max(arr[N-1]), min(mini[N-1]))
