6ad2a8e495d92a8557953bfe95e23039a2cb2f2d
ejuun <122511545+ejuun@users.noreply.github.com>
2023-08-08 22:52:31 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 80 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+	public static void main (String [] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String word = new String(br.readLine());
+		System.out.println(word.length());
+	}
+	}
