76de9cd35e3eb0d79660c81a7298ec2469311c14
ejuun <122511545+ejuun@users.noreply.github.com>
2023-08-09 22:14:10 +0900

    [Bronze V] Title: 합, Time: 88 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,16 @@
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+	public static void main (String [] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int s = 0;
+		for (int i = 1; i <= N; i++) {
+			s += i;
+		}
+		System.out.println(s);
+	}
+	}
