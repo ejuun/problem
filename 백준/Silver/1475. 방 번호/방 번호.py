4be4d15cfc14a408eef53fffc4f4808cd0ba55a1
ejuun <122511545+ejuun@users.noreply.github.com>
2023-08-13 17:44:42 +0900

    [Silver V] Title: 방 번호, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,11 @@
+num = int(input())
+cnt = [0] * 10
+for i in str(num):
+    if i == '6' or i == '9':
+        if cnt[6] == cnt[9]:
+            cnt[6] += 1
+        else:
+            cnt[9] += 1
+    else:
+        cnt[int(i)] += 1
+print(max(cnt))
