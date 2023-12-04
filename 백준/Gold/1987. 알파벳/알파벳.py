d74e0125e02285a9f97c7d0672484f6d73a91b0e
ejuun <122511545+ejuun@users.noreply.github.com>
2023-12-04 21:47:40 +0900

    [Gold IV] Title: 알파벳, Time: 7796 ms, Memory: 155628 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,31 +1,24 @@
-def dfs(x, y):
-    global ans
+import sys
+input = sys.stdin.readline
 
-    if ans == 26:
-        return
+R, C = map(int, input().split())
+arr = [list(map(str, input().rstrip())) for _ in range(R)]
+sets = set()
+sets.add(arr[0][0])
+move = 0
+
+def dfs(x, y, cnt):
+    global move
+    move = max(cnt, move)
 
     for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
         nx = x + dx
         ny = y + dy
         if 0 <= nx < R and 0 <= ny < C:
-            if not visited[nx][ny] and arr[nx][ny] not in sets:
-                visited[nx][ny] = 1
+            if arr[nx][ny] not in sets:
                 sets.add(arr[nx][ny])
-                if ans < len(sets):
-                    ans = len(sets)
-                dfs(nx, ny)
+                dfs(nx, ny, cnt + 1)
                 sets.remove(arr[nx][ny])
-                visited[nx][ny] = 0
 
-R, C = map(int, input().split())
-arr = [list(map(str, input())) for _ in range(R)]
-
-visited = [[0 for _ in range(C)] for _ in range(R)]
-visited[0][0] = 1
-
-sets = set()
-sets.add(arr[0][0])
-ans = 1
-
-dfs(0, 0)
-print(ans)
+dfs(0, 0, 1)
+print(move)
