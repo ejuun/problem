fb5420cd53cf3925071dcd28a016b7d9b4db8843
ejuun <122511545+ejuun@users.noreply.github.com>
2024-05-18 22:17:29 +0900

    [Silver V] Title: 소수, Time: 88 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/소수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			A = (A%B) * 10;
+			ans = A / B;
+		}
+		System.out.println(ans);
+	} 
+}
