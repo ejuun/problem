cc5696aa870b06c6b9d03772d850a55bcbd3ad72
ejuun <122511545+ejuun@users.noreply.github.com>
2024-07-05 23:13:35 +0900

    [Silver V] Title: 피자 (Small), Time: 116 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Silver/14606. 피자 （Small）/피자 （Small）.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+	        Scanner scanner = new Scanner(System.in);
+	        int N = scanner.nextInt();
+	        System.out.println(N*(N-1)/2);
+}
+}
