1b7aea1e9713944e5d56cea5dcd91e270e43b72f
ejuun <122511545+ejuun@users.noreply.github.com>
2024-08-10 16:46:39 +0900

    [Silver V] Title: 인기 투표, Time: 132 ms, Memory: 110476 KB -BaekjoonHub

---
+++ 백준/Silver/11637. 인기 투표/인기 투표.py
@@ -0,0 +1,14 @@
+def popularity_vote():
+    arr = [int(input()) for _ in range(int(input()))]
+    max_val = max(arr)
+
+    if arr.count(max_val) > 1:
+        print("no winner")
+        return
+
+    idx = arr.index(max_val) + 1
+    print('majority winner', idx) if sum(arr) - max_val < max_val else print('minority winner', idx)
+
+
+for _ in range(int(input())):
+    popularity_vote()
